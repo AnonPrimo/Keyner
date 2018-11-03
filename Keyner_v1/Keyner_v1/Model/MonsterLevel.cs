3aa6986c5b2eb0afff845a0c46985420defc67a3
Viryz <viryz1998@gmail.com>
2018-11-03 20:52:44 +0200

    Models done

---
+++ Keyner_v1/Keyner_v1/Model/MonsterLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Keyner_v1.Model
+{
+    class MonsterLevel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Id_Monster { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ Keyner_v1/Keyner_v1/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyner_v1.Model
+{
+    [Table("Users")]
+    class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public int Id_Group { get; set; }
+
+        public int Id_Monster { get; set; }
+
+        public int Money { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
