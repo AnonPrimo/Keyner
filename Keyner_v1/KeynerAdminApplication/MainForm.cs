17d1039eb399f32c9db2522ccf633dae2222c94f
Viryz <viryz1998@gmail.com>
2018-11-04 19:34:07 +0200

    Add admin application

---
+++ Keyner_v1/KeynerAdminApplication/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeynerAdminApplication
+{
+    public partial class MainForm : Form
+    {
+        private List<Model.Test> _testList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            using (Model.KeynerContext db = new Model.KeynerContext())
+            {
+                _testList = db.TestSet.ToList();
+            }
+            int index = 0;
+            foreach (Model.Test item in _testList)
+            {
+                dataGridViewTests.Rows.Add(item.Id, item.CountMistakes, item.BestTime);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormNewTest form = new FormNewTest();
+            form.ShowDialog();
+        }
+    }
+}
--- Keyner_v1/KeynerAdminApplication/Model/Purchase.cs
+++ Keyner_v1/KeynerAdminApplication/Model/Purchase.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Keyner_v1.Model
+namespace KeynerAdminApplication.Model
 {
     public class Purchase
     {
