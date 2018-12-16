dba7eb9f30584ddbd7415fae5bc19638bd4745c0
wispeckiy <max.vitvitckiy@gmail.com>
2018-12-16 21:03:52 +0200

    test complete

--- Keyner_v1/Keyner_v1/Controller/TestController.cs
+++ Keyner_v1/Keyner_v1/Controller/TestController.cs
@@ -12,6 +12,7 @@ namespace Keyner_v1.Controller
     {
         public string CurrentStr { get; set; }
         public int RepeatCount { get; set; }
+       public static List<Char> collection = new List<Char>();
 
         public TestController()
         {
@@ -20,9 +21,21 @@ namespace Keyner_v1.Controller
 
         public void myMethod()
         {
-            CurrentStr = "а а а а а а а а о о о о о о о о";
+            CurrentStr = "а а а о о";
             RepeatCount = 3;
+
+            for (int i = 0; i < RepeatCount; i++)
+            {
+                for (int j = 0; j < CurrentStr.Length; j++)
+                {
+                    collection.Add(CurrentStr[j]);
+                }
+            }
         }
-        
+
+        //public List<Char> GetCollection()
+        //{
+        //    return collection;
+        //}
     }
 }
